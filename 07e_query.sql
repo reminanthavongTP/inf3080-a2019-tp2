6a542c3b92a2c04b13c8e895f522d798263ef5d8
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 13:41:26 -0500

    Update 07e_query.sql

--- 07e_query.sql
+++ 07e_query.sql
@@ -4,3 +4,10 @@ SET ECHO ON
 -- Écrire la commande qui donne le droit d'exécution sur la fonction f_prix(...);
 -- Écrire la commande qui donne le droit de modification sur la table TypeEquipement;
 SET ECHO ON
+
+GRANT SELECT ON v_tables TO francoeur_g
+/
+GRANT EXECUTE ON f_prix() TO francoeur_g
+/
+GRANT ALL PRIVILEGES ON tp1TypeEquipement TO francoeur_g
+/
