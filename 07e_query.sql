a115ccd8a889fbaa3ea0ea6c8ac6257c11e0c722
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 12:41:16 -0500

    Create 07e_query.sql

---
+++ 07e_query.sql
@@ -0,0 +1,6 @@
+SET ECHO ON
+-- Donner des droits à l'usager francoeur_g
+-- Écrire la commande qui donne le droit lecture sur la table compagnie et la vue v_tables;
+-- Écrire la commande qui donne le droit d'exécution sur la fonction f_prix(...);
+-- Écrire la commande qui donne le droit de modification sur la table TypeEquipement;
+SET ECHO ON
