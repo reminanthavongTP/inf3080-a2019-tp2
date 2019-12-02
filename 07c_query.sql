fda0e358a60a0b2070cd0fc02515b15139d361ce
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 12:38:48 -0500

    Update and rename 04c_query.sql to 07c_query.sql

---
+++ 07c_query.sql
@@ -0,0 +1,13 @@
+SET ECHO ON
+-- Écrire le code pour créer une vue nommée v_tables qui liste les tables et le nombre de lignes que chacune contient. 
+-- Dans ce script (fichier) vous devez faire, écrire, tout ce qui est nécessaire pour arriver à un résultat exact.
+SET ECHO ON
+SELECT
+  LOWER(table_name), owner
+FROM
+  all_tables
+WHERE
+  table_name LIKE 'TP1%'
+ORDER BY
+  owner, table_name DESC
+/
