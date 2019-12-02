7ad2054fbae396e5c25e301a9fe98e60ffa7af56
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 11:29:32 -0500

    Update 04_procedure.sql

--- 04_procedure.sql
+++ 04_procedure.sql
@@ -3,100 +3,32 @@ SET ECHO ON
 -- Version sans accents
 -- p_demande() p_soumission() p_commande() f_prix()
 SET ECHO ON
-CREATE SEQUENCE tp2Client
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 100
- cycle
+CREATE OR REPLACE PROCEDURE p_demande()
+(IS
+ BEGIN
+ dbms_output.put_line (‘Welcome '|| p_name);
+ END
 )
 /
-CREATE SEQUENCE tp2DemandeSoumission
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 100
- cycle
+CREATE OR REPLACE PROCEDURE p_soumission()
+(IS
+ BEGIN
+ dbms_output.put_line (‘Welcome '|| p_name);
+ END
 )
 /
-CREATE SEQUENCE tp2SoumissionE
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 100
- cycle
+CREATE OR REPLACE PROCEDURE p_commande()
+(IS
+ BEGIN
+ dbms_output.put_line (‘Welcome '|| p_name);
+ END
 )
 /
-CREATE SEQUENCE tp2Chargement
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 200
- cycle
-)
-/
-CREATE SEQUENCE tp2Route
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 20
- cycle
-)
-/
-CREATE SEQUENCE tp2Carburant
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 12
- cycle
-)
-/
-CREATE SEQUENCE tp2Compagnie
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 20
- cycle
-)
-/
-CREATE SEQUENCE tp2Camion
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 40
- cycle
-)
-/
-CREATE SEQUENCE tp2TypeEquipement
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 40
- cycle
-)
-/
-CREATE SEQUENCE tp2Equipement
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 40
- cycle
-)
-/
-CREATE SEQUENCE tp2Position
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 40
- cycle
-)
-/
-CREATE SEQUENCE tp2Tracteur
-(start with 10
- increment by 2
- minvalue 10
- maxvalue 40
- cycle
+CREATE OR REPLACE FUNCTION f_prix() RETURN VAR.CHAR2
+(IS
+ BEGIN
+ RETURN (‘Welcome ‘|| p_name);
+ END
 )
 /
 COMMIT
