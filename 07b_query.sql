cb1718cd9ffde805aaad2de4dbff246c196984e5
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 13:26:46 -0500

    Update 07b_query.sql

--- 07b_query.sql
+++ 07b_query.sql
@@ -2,9 +2,101 @@ SET ECHO ON
 -- Écrire le code nécessaire pour réinitialiser toutes les séquences qui existent dans la base de données
 -- La séquence offrira comme première valeur 1 et augmentera de 1.
 SET ECHO ON
-SELECT tp1Compagnie.nomcompagnie, tp1camion.pcamion, tp1position.cposition, tp1position.ndisponible 
-FROM ((tp1Compagnie
-INNER JOIN tp1camion ON tp1Compagnie.pcompagnie = tp1camion.pcompagnie)
-INNER JOIN tp1position ON tp1camion.pcamion = tp1position.pcamion)
-WHERE tp1position.ndisponible = 0
+CREATE OR REPLACE SEQUENCE tp2Client
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 100
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2DemandeSoumission
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 100
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2SoumissionE
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 100
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2Chargement
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 200
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2Route
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 20
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2Carburant
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 12
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2Compagnie
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 20
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2Camion
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 40
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2TypeEquipement
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 40
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2Equipement
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 40
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2Position
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 40
+ cycle
+)
+/
+CREATE OR REPLACE SEQUENCE tp2Tracteur
+(start with 1
+ increment by 1
+ minvalue 10
+ maxvalue 40
+ cycle
+)
+/
+COMMIT
 /
