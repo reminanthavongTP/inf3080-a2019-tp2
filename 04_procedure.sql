a7e646beea0cab0aa7e4c912667f86f3c4625e7c
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 12:15:12 -0500

    Update 04_procedure.sql

--- 04_procedure.sql
+++ 04_procedure.sql
@@ -3,31 +3,38 @@ SET ECHO ON
 -- Version sans accents
 -- p_demande() p_soumission() p_commande() f_prix()
 SET ECHO ON
-CREATE OR REPLACE PROCEDURE p_demande()
+CREATE OR REPLACE PROCEDURE p_demande(pClient, nLatOri, nLongOri, nLatDes, nLongDes, nPoids, nLargeur, nLongueur, nHauteur)
+(@pChargement 		INTEGER 	= '0',
+ @cChargement 		VARCHAR(30) 	= pClient + pChargement
+ AS
+ BEGIN
+    INSERT INTO tp1Chargement (pChargement,cChargement,nLatOri,nLongOri,nLatDes,nLongDes,nLongueur,nLargeur,nHauteur,nPoids,pClient) 
+    values (@pChargement,@cChargement,nLatOri,nLongOri,nLatDes,nLongDes,nLongueur,nLargeur,nHauteur,nPoids,pClient)
+ END
+)
+/
+CREATE OR REPLACE PROCEDURE p_soumission(pChargement)
 (IS
  BEGIN
  dbms_output.put_line (‘Welcome '|| p_name);
  END
 )
 /
-CREATE OR REPLACE PROCEDURE p_soumission()
+CREATE OR REPLACE PROCEDURE p_commande(pSoumissionD)
 (IS
  BEGIN
  dbms_output.put_line (‘Welcome '|| p_name);
  END
 )
 /
-CREATE OR REPLACE PROCEDURE p_commande()
+CREATE OR REPLACE FUNCTION f_prix(pSoumissionD) RETURN NUMBER
 (IS
+ prix INTEGER;
  BEGIN
- dbms_output.put_line (‘Welcome '|| p_name);
- END
-)
-/
-CREATE OR REPLACE FUNCTION f_prix() RETURN VAR.CHAR2
-(IS
- BEGIN
- RETURN (‘Welcome ‘|| p_name);
+ prix = tp1Tracteur.nConsommation * tp1Chargement.nDistance * tp1Chargement.nCout; 
+ prix = prix + (tp1TypeEquipement.nCount * tp1Chargement.nDistance); 
+ prix = prix * tp1Compagnie.nProfit;
+ RETURN prix;
  END
 )
 /
