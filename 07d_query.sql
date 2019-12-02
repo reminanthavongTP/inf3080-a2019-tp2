0d1ef12c4cb760cfee295942187557e8627804cf
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 12:39:43 -0500

    Update and rename 04d_query.sql to 07d_query.sql

--- 07d_query.sql
+++ 07d_query.sql
@@ -1,5 +1,7 @@
 SET ECHO ON
--- Une requête qui retourne les attributs des entités E = { Tracteur, Camion, Equipement }
+-- Écrire le les intructions nécessaire, sous la forme d'un tutoriel markdown (étapes), 
+-- pour afin d'inclure dans la base de données le programme Java f_prix2( ... ) qui est disponible dans la classe Transport. 
+-- L'implémentation de la classe est dans le fichier Transport.java.
 SET ECHO ON
 CREATE OR REPLACE VIEW entites AS
 SELECT tp1camion.tracteur, tp1camion.pcamion, tp1camion.pcompagnie,tp1camion.lenght,tp1camion.width,tp1camion.height,tp1camion.capacity,tp1typeequipement.ctypeequipement 
