2b0750b33e759f6d5828231ef5bcc94fe5baaccc
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 13:31:41 -0500

    Update 07c_query.sql

--- 07c_query.sql
+++ 07c_query.sql
@@ -2,12 +2,16 @@ SET ECHO ON
 -- Écrire le code pour créer une vue nommée v_tables qui liste les tables et le nombre de lignes que chacune contient. 
 -- Dans ce script (fichier) vous devez faire, écrire, tout ce qui est nécessaire pour arriver à un résultat exact.
 SET ECHO ON
-SELECT
-  LOWER(table_name), owner
-FROM
-  all_tables
-WHERE
-  table_name LIKE 'TP1%'
-ORDER BY
-  owner, table_name DESC
-/
+
+CREATE OR REPLACE VIEW v_tables AS
+  SELECT
+   LOWER(table_name), owner
+  FROM
+   all_tables
+  WHERE
+   table_name LIKE 'TP1%'
+  ORDER BY
+   owner, table_name DESC
+  
+/  
+
