962c4c19104408c148520bef216924f459a3ce69
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 00:13:12 -0500

    Update 01_schema.sql

--- 01_schema.sql
+++ 01_schema.sql
@@ -8,9 +8,13 @@ DROP TABLE tp1Client CASCADE CONSTRAINTS
 /
 DROP TABLE tp1DemandeSoumission CASCADE CONSTRAINTS
 /
+DROP TABLE tp1SoumissionE CASCADE CONSTRAINTS
+/
+DROP TABLE tp1Chargement CASCADE CONSTRAINTS
+/
 DROP TABLE tp1Route CASCADE CONSTRAINTS
 /
-DROP TABLE tp1Proposition CASCADE CONSTRAINTS
+DROP TABLE tp1Carburant CASCADE CONSTRAINTS
 /
 DROP TABLE tp1Compagnie CASCADE CONSTRAINTS
 /
@@ -18,41 +22,28 @@ DROP TABLE tp1Camion CASCADE CONSTRAINTS
 /
 DROP TABLE tp1TypeEquipement CASCADE CONSTRAINTS
 /
+DROP TABLE tp1Equipement CASCADE CONSTRAINTS
+/
 DROP TABLE tp1Position CASCADE CONSTRAINTS
+/
+DROP TABLE tp1Tracteur CASCADE CONSTRAINTS
 /
 CREATE TABLE tp1Client
 (pClient 		INTEGER 		NOT NULL,
- nomClient 		VARCHAR(20) 	NOT NULL,
- prenomClient 		VARCHAR(20) 	NOT NULL,
- noTelephone 	VARCHAR(15) 	NOT NULL,
- email 	VARCHAR(50) 	NOT NULL,
+ cClient 		VARCHAR(20) 	NOT NULL,
  PRIMARY KEY 	(pClient)
 )
 /
 CREATE TABLE tp1DemandeSoumission
 (pSoumission 		INTEGER 		NOT NULL,
- origine 		VARCHAR(20) 	NOT NULL,
- destination 		VARCHAR(20) 	NOT NULL,
- dateSoumission 	DATE 	NOT NULL,
- refrigerated 		INTEGER	NOT NULL,
- dock 		INTEGER 	NOT NULL,
- hazardous 	INTEGER 	NOT NULL,
- rush 		INTEGER 		NOT NULL,
- lenght		FLOAT 	NOT NULL,
- width 		FLOAT  	NOT NULL,
- height 	FLOAT  	NOT NULL,
- quantity 	INTEGER  	NOT NULL,
- hours 	FLOAT  	NOT NULL,
- valeur 	INTEGER  	NOT NULL,
- pClient 		INTEGER 		NOT NULL,
- CHECK (quantity>0),
+ nPrix 		INTEGER 		NOT NULL,
+ pCamion 		INTEGER 		NOT NULL,
  PRIMARY KEY 	(pSoumission),
- FOREIGN KEY 	(pClient) REFERENCES tp1Client
+ FOREIGN KEY 	(pCamion) REFERENCES tp1Camion
 )
 /
 CREATE TABLE tp1Route
-(pSoumission 		INTEGER 		NOT NULL,
- pRoute 		VARCHAR(30) 	NOT NULL,
+(pRoute 		VARCHAR(30) 	NOT NULL,
  cRoute 		VARCHAR(30) 	NOT NULL,
  nLatOri 	FLOAT(8) 	NOT NULL,
  nLongOri 	FLOAT(8) 	NOT NULL,
@@ -60,66 +51,70 @@ CREATE TABLE tp1Route
  nLongDes 		FLOAT(8) 		NOT NULL,
  nDistance 		FLOAT(8) 		NOT NULL,
  CHECK (nDistance>0),
- PRIMARY KEY 	(pRoute),
- FOREIGN KEY 	(pSoumission) REFERENCES tp1DemandeSoumission
+ PRIMARY KEY 	(pRoute)
 )
 /
 CREATE TABLE tp1Compagnie
 (pCompagnie 		INTEGER 		NOT NULL,
- nomCompagnie 		VARCHAR(20) 	NOT NULL,
- noTelephone 	VARCHAR(15) 	NOT NULL,
- email 	VARCHAR(50) 	NOT NULL,
- nbCamion INTEGER NOT NULL, 
- CHECK (nbCamion>=1),
+ cCompagnie 		VARCHAR(20) 	NOT NULL,
+ nProfit      INTEGER      NOT NULL,
  PRIMARY KEY 	(pCompagnie)
 )
 /
 CREATE TABLE tp1Camion
 (pCamion 		INTEGER 		NOT NULL,
  pCompagnie 		INTEGER 		NOT NULL,
- lenght		FLOAT 	NOT NULL,
- width 		FLOAT  	NOT NULL,
- height 	FLOAT  	NOT NULL,
- capacity	FLOAT  	NOT NULL,
- tracteur INTEGER NOT NULL,
+ pEquipement 		INTEGER 		NOT NULL,
+ pTracteur 		INTEGER 		NOT NULL,
+ cCamion      VARCHAR(20) 	NOT NULL,
  PRIMARY KEY 	(pCamion),
- FOREIGN KEY 	(pCompagnie) REFERENCES tp1Compagnie
+ FOREIGN KEY 	(pCompagnie) REFERENCES tp1Compagnie,
+ FOREIGN KEY 	(pEquipement) REFERENCES tp1Equipement,
+ FOREIGN KEY 	(pTracteur) REFERENCES tp1Tracteur
 )
 /
 CREATE TABLE tp1TypeEquipement
-(pCamion 		INTEGER 		NOT NULL,
- pTypeEquipement INTEGER NOT NULL,
+(pTypeEquipement INTEGER NOT NULL,
  cTypeEquipement VARCHAR(30) NOT NULL,
  nCout FLOAT(8) NOT NULL,
- PRIMARY KEY    (pTypeEquipement),
- FOREIGN KEY 	(pCamion) REFERENCES tp1Camion
+ PRIMARY KEY    (pTypeEquipement)
 )
 /
-CREATE TABLE tp1Position
-(pCamion 		INTEGER 		NOT NULL,
- pPosition		VARCHAR(30) 	NOT NULL,
- cPosition 		VARCHAR(30) 	NOT NULL,
- nLat 	FLOAT(8) 	NOT NULL,
- nLong  FLOAT(8)	NOT NULL,
- nDisponible		INTEGER		NOT NULL,
- PRIMARY KEY    (pPosition),
- FOREIGN KEY 	(pCamion) REFERENCES tp1Camion
-)
-/
-CREATE TABLE tp1Proposition
-(pProposition 		INTEGER 		NOT NULL,
- pSoumission 		INTEGER 		NOT NULL,
- datePickup 	DATE 	NOT NULL,
- dateDelivery 		DATE 		NOT NULL,
- prixLivraison		FLOAT(6) 		NOT NULL,
+CREATE TABLE tp1Equipement
+(pEquipement INTEGER NOT NULL,
+ cEquipement VARCHAR(30) NOT NULL,
+ nCapacite INTEGER NOT NULL,
+ nLongueur INTEGER NOT NULL,
+ nLargeur INTEGER NOT NULL,
+ nHauteur INTEGER NOT NULL,
  pTypeEquipement INTEGER NOT NULL,
- prixCarburant 	FLOAT(8) 	NOT NULL,
- consommation 		FLOAT(8) 		NOT NULL,
- margeProfit		FLOAT(3) 		DEFAULT 1.18,
- CHECK (datePickup<=dateDelivery),
- PRIMARY KEY 	(pProposition),
- FOREIGN KEY 	(pSoumission) REFERENCES tp1DemandeSoumission,
+ PRIMARY KEY    (pTypeEquipement),
  FOREIGN KEY 	(pTypeEquipement) REFERENCES tp1TypeEquipement
+)
+/
+CREATE TABLE tp1SoumissionE
+(pSoumissionE		INTEGER 		NOT NULL,
+ pChargement   INTEGER 		NOT NULL,
+ dSoumission   INTEGER 		NOT NULL,
+ PRIMARY KEY 	(pSoumissionE),
+ FOREIGN KEY 	(pChargement) REFERENCES tp1Chargement
+)
+/
+CREATE TABLE tp1Tracteur
+(pTracteur INTEGER NOT NULL,
+ cTracteur VARCHAR(30) NOT NULL,
+ nConsommation FLOAT(8) NOT NULL,
+ bActif        INTEGER NOT NULL,
+ pCarburant    INTEGER NOT NULL,
+ PRIMARY KEY    (pTypeEquipement),
+ FOREIGN KEY 	(pCarburant) REFERENCES tp1Carburant
+)
+/
+CREATE TABLE tp1Carburant
+(pCarburant INTEGER NOT NULL,
+ cCarburantt VARCHAR(30) NOT NULL,
+ nCout INTEGER NOT NULL,
+ PRIMARY KEY    (pCarburant)
 )
 /
 COMMIT
