3ab0451e2237cb2510453a1f75a85043797ec2bc
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 10:54:47 -0500

    Update 02_sequence.sql

--- 02_sequence.sql
+++ 02_sequence.sql
@@ -4,96 +4,96 @@ SET ECHO ON
 -- INSERTION dans les tables
 SET ECHO ON
 CREATE SEQUENCE tp2Client
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2DemandeSoumission
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2SoumissionE
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2Chargement
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2Route
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2Carburant
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2Compagnie
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2Camion
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2TypeEquipement
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2Equipement
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2Position
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
 )
 /
 CREATE SEQUENCE tp2Tracteur
-(start with 1
- increment by 1
+(start with 10
+ increment by 2
  minvalue 0
  maxvalue 100
  cycle
