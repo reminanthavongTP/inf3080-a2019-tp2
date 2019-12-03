4fb93e66e16cd801e079385cd317dac95f0254d8
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-02 23:58:42 -0500

    Update 01_schema.sql

--- 01_schema.sql
+++ 01_schema.sql
@@ -35,6 +35,62 @@ CREATE TABLE tp1Client
  PRIMARY KEY 	(pClient)
 )
 /
+CREATE TABLE tp1Compagnie
+(pCompagnie 		INTEGER 		NOT NULL,
+ cCompagnie 		VARCHAR(20) 	NOT NULL,
+ nProfit      NUMBER      NOT NULL,
+ PRIMARY KEY 	(pCompagnie)
+)
+/
+CREATE TABLE tp1TypeEquipement
+(pTypeEquipement INTEGER NOT NULL,
+ cTypeEquipement VARCHAR(30) NOT NULL,
+ nCout NUMBER(8,5) NOT NULL,
+ CHECK           (cTypeEquipement IN ( 'Drybox','Flatbed')),
+ PRIMARY KEY    (pTypeEquipement)
+)
+/
+CREATE TABLE tp1Equipement
+(pEquipement INTEGER NOT NULL,
+ cEquipement VARCHAR(30) NOT NULL,
+ nCapacite NUMBER NOT NULL,
+ nLongueur NUMBER NOT NULL,
+ nLargeur NUMBER NOT NULL,
+ nHauteur NUMBER NOT NULL,
+ pTypeEquipement INTEGER NOT NULL,
+ PRIMARY KEY    (pTypeEquipement),
+ FOREIGN KEY 	(pTypeEquipement) REFERENCES tp1TypeEquipement
+)
+/
+CREATE TABLE tp1Carburant
+(pCarburant INTEGER NOT NULL,
+ cCarburant VARCHAR(30) NOT NULL,
+ nCout NUMBER NOT NULL,
+ PRIMARY KEY    (pCarburant)
+)
+/
+CREATE TABLE tp1Tracteur
+(pTracteur INTEGER NOT NULL,
+ cTracteur VARCHAR(30) NOT NULL,
+ nConsommation FLOAT(8) NOT NULL,
+ bActif        INTEGER NOT NULL,
+ pCarburant    INTEGER NOT NULL,
+ PRIMARY KEY    (pTracteur),
+ FOREIGN KEY 	(pCarburant) REFERENCES tp1Carburant
+)
+/
+CREATE TABLE tp1Camion
+(pCamion 		INTEGER 		NOT NULL,
+ pCompagnie 		INTEGER 		NOT NULL,
+ pEquipement 		INTEGER 		NOT NULL,
+ pTracteur 		INTEGER 		NOT NULL,
+ cCamion      VARCHAR(20) 	NOT NULL,
+ PRIMARY KEY 	(pCamion),
+ FOREIGN KEY 	(pCompagnie) REFERENCES tp1Compagnie,
+ FOREIGN KEY 	(pEquipement) REFERENCES tp1Equipement,
+ FOREIGN KEY 	(pTracteur) REFERENCES tp1Tracteur
+)
+/
 CREATE TABLE tp1DemandeSoumission
 (pSoumission 		INTEGER 		NOT NULL,
  nPrix 		NUMBER 		NOT NULL,
@@ -54,79 +110,6 @@ CREATE TABLE tp1Route
  PRIMARY KEY 	(pRoute)
 )
 /
-CREATE TABLE tp1Compagnie
-(pCompagnie 		INTEGER 		NOT NULL,
- cCompagnie 		VARCHAR(20) 	NOT NULL,
- nProfit      NUMBER      NOT NULL,
- PRIMARY KEY 	(pCompagnie)
-)
-/
-CREATE TABLE tp1Camion
-(pCamion 		INTEGER 		NOT NULL,
- pCompagnie 		INTEGER 		NOT NULL,
- pEquipement 		INTEGER 		NOT NULL,
- pTracteur 		INTEGER 		NOT NULL,
- cCamion      VARCHAR(20) 	NOT NULL,
- PRIMARY KEY 	(pCamion),
- FOREIGN KEY 	(pCompagnie) REFERENCES tp1Compagnie,
- FOREIGN KEY 	(pEquipement) REFERENCES tp1Equipement,
- FOREIGN KEY 	(pTracteur) REFERENCES tp1Tracteur
-)
-/
-CREATE TABLE tp1TypeEquipement
-(pTypeEquipement INTEGER NOT NULL,
- cTypeEquipement VARCHAR(30) NOT NULL,
- nCout NUMBER(8,5) NOT NULL,
- CHECK           (cTypeEquipement IN ( 'Drybox','Flatbed'))
- PRIMARY KEY    (pTypeEquipement)
-)
-/
-CREATE TABLE tp1Equipement
-(pEquipement INTEGER NOT NULL,
- cEquipement VARCHAR(30) NOT NULL,
- nCapacite NUMBER NOT NULL,
- nLongueur NUMBER NOT NULL,
- nLargeur NUMBER NOT NULL,
- nHauteur NUMBER NOT NULL,
- pTypeEquipement INTEGER NOT NULL,
- PRIMARY KEY    (pTypeEquipement),
- FOREIGN KEY 	(pTypeEquipement) REFERENCES tp1TypeEquipement
-)
-/
-CREATE TABLE tp1SoumissionE
-(pSoumissionE		INTEGER 		NOT NULL,
- pChargement   INTEGER 		NOT NULL,
- dSoumission   DATE 		NOT NULL,
- PRIMARY KEY 	(pSoumissionE),
- FOREIGN KEY 	(pChargement) REFERENCES tp1Chargement
-)
-/
-CREATE TABLE tp1Tracteur
-(pTracteur INTEGER NOT NULL,
- cTracteur VARCHAR(30) NOT NULL,
- nConsommation FLOAT(8) NOT NULL,
- bActif        INTEGER NOT NULL,
- pCarburant    INTEGER NOT NULL,
- PRIMARY KEY    (pTypeEquipement),
- FOREIGN KEY 	(pCarburant) REFERENCES tp1Carburant
-)
-/
-CREATE TABLE tp1Carburant
-(pCarburant INTEGER NOT NULL,
- cCarburant VARCHAR(30) NOT NULL,
- nCout NUMBER NOT NULL,
- PRIMARY KEY    (pCarburant)
-)
-/
-CREATE TABLE tp1Position
-(pPosition INTEGER NOT NULL,
- cPosition VARCHAR(30) NOT NULL,
- nLat NUMBER(8,5) NOT NULL,
- nLong NUMBER(8,5) NOT NULL,
- bDisponible INTEGER NOT NULL,
- PRIMARY KEY    (pPosition)
-)
-/
 CREATE TABLE tp1Chargement
 (pChargement 		INTEGER 	NOT NULL,
  cChargement 		VARCHAR(30) 	NOT NULL,
@@ -141,6 +124,23 @@ CREATE TABLE tp1Chargement
  pClient 		INTEGER 		NOT NULL,
  PRIMARY KEY 	(pChargement),
  FOREIGN KEY 	(pClient) REFERENCES tp1Client
+)
+/                  
+CREATE TABLE tp1SoumissionE
+(pSoumissionE		INTEGER 		NOT NULL,
+ pChargement   INTEGER 		NOT NULL,
+ dSoumission   DATE 		NOT NULL,
+ PRIMARY KEY 	(pSoumissionE),
+ FOREIGN KEY 	(pChargement) REFERENCES tp1Chargement
+)
+/
+CREATE TABLE tp1Position
+(pPosition INTEGER NOT NULL,
+ cPosition VARCHAR(30) NOT NULL,
+ nLat NUMBER(8,5) NOT NULL,
+ nLong NUMBER(8,5) NOT NULL,
+ bDisponible INTEGER NOT NULL,
+ PRIMARY KEY    (pPosition)
 )
 /
 COMMIT
