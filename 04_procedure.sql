bfc972be419ce358b2ecf284c1229085b5aa6e41
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-04 12:17:48 -0500

    Update 04_procedure.sql

--- 04_procedure.sql
+++ 04_procedure.sql
@@ -31,5 +31,34 @@ RAISE_APPLICATION_ERROR(-20001,'erreur interne');
 END afficherLivraisons
 )
 /
+CREATE OR REPLACE PROCEDURE ProduireFacture
+(
+(Soumission tp1DemandeSoumission.pSoumission%TYPE) 
+IS
+laSoumission tp1DemandeSoumission.pSoumission%TYPE;
+-- Déclaration d'un curseur (CURSOR) PL/SQL pour itérer sur les lignes
+CURSOR lignesDetail
+(unSoumission tp1DemandeSoumission.pSoumission%TYPE)
+IS
+SELECT pSoumission, pCamion, dateSoumission
+FROM tp1DemandeSoumission
+WHERE Soumission = unSoumission;
+BEGIN
+DBMS_OUTPUT.PUT('noSoumission #:');
+DBMS_OUTPUT.PUT_LINE(Soumission);
+OPEN lignesDetail(Soumission);
+LOOP
+FETCH lignesDetail INTO laSoumission;
+EXIT WHEN lignesDetail%NOTFOUND;
+DBMS_OUTPUT.PUT('noSoumission #:');
+DBMS_OUTPUT.PUT(laSoumission);
+END LOOP;
+CLOSE lignesDetail ;
+EXCEPTION
+WHEN OTHERS THEN
+RAISE_APPLICATION_ERROR(-20001,'erreur interne');
+END afficherLivraisons
+)
+/
 COMMIT
 /
