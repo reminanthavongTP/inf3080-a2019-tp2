fb3a768d6fc08db8a42d75cd582d090204c1bf3d
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-04 22:53:49 -0500

    Update 02_sequence.sql

--- 02_sequence.sql
+++ 02_sequence.sql
@@ -28,100 +28,100 @@ DROP SEQUENCE tp2Position
 DROP SEQUENCE tp2Tracteur
 /
 CREATE SEQUENCE tp2Client
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 100
  cycle
-)
+
 /
 CREATE SEQUENCE tp2DemandeSoumission
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 100
  cycle
-)
+
 /
 CREATE SEQUENCE tp2SoumissionE
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 100
  cycle
-)
+
 /
 CREATE SEQUENCE tp2Chargement
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 200
  cycle
-)
+
 /
 CREATE SEQUENCE tp2Route
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 20
  cycle
-)
+
 /
 CREATE SEQUENCE tp2Carburant
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 12
  cycle
-)
+
 /
 CREATE SEQUENCE tp2Compagnie
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 20
  cycle
-)
+
 /
 CREATE SEQUENCE tp2Camion
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 40
  cycle
-)
+
 /
 CREATE SEQUENCE tp2TypeEquipement
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 40
  cycle
-)
+
 /
 CREATE SEQUENCE tp2Equipement
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 40
  cycle
-)
+
 /
 CREATE SEQUENCE tp2Position
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 40
  cycle
-)
+
 /
 CREATE SEQUENCE tp2Tracteur
-(start with 10
+ start with 10
  increment by 2
  minvalue 10
  maxvalue 40
  cycle
-)
+
 /
 COMMIT
 /
