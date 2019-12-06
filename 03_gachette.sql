360be06ab1b3df3f71313fa5fe5160883716027e
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-06 00:25:47 -0500

    Update 03_gachette.sql

--- 03_gachette.sql
+++ 03_gachette.sql
@@ -131,26 +131,5 @@ BEGIN
   :new.pCarburant := tp2Carburant.currval;
 END;
 /
-create OR REPLACE trigger tp2ReduireCamion
-BEFORE INSERT ON tp1SoumissionE
-FOR EACH ROW
-DECLARE
-  rpCompagnie  tp1Camion.pCompagnie %TYPE;
-BEGIN
-
-SELECT tp1Camion.pCompagnie INTO rpCompagnie
-     FROM tp1SoumissionE JOIN tp1Chargement
-     ON tp1SoumissionE.pChargement = tp1Chargement.pChargement
-     JOIN tp1DemandeSoumission
-     ON tp1Chargement.pSoumission = tp1DemandeSoumission.pSoumission
-     JOIN tp1Camion
-     ON tp1DemandeSoumission.pCamion = tp1Camion.pCamion
-     WHERE tp1SoumissionE.pSoumissionE = :new.pSoumissionE;
-
-  UPDATE tp1Compagnie
-     SET tp1Compagnie.nCamion = tp1Compagnie.nCamion - 1
-     WHERE tp1Compagnie.pCompagnie = rpCompagnie;
-END;     
-/
 COMMIT
 /
