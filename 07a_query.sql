3a638d13c40609fff8346bc7c4940f3f2c89f39c
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-06 20:56:51 -0500

    Delete 07a_query.sql

--- 07a_query.sql
+++
@@ -1,5 +0,0 @@
-SET ECHO ON
--- Écrire (un script) le code pour effacer tous les tuples de toutes les tables qui sont dans la base de données;
-SET ECHO ON
-select pSoumission,origine,destination from tp1DemandeSoumission where pclient = 4 and datesoumission = '2019/09/30'
-/
