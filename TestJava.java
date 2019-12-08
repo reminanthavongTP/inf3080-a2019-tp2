3cbc437c960087fc493746899a6cbfa229da9997
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-07 21:13:06 -0500

    Create TestJava.java

---
+++ TestJava.java
@@ -0,0 +1,33 @@
+// Exemple de programme JAVA qui utilise le pilote JDBC OCI8 d'Oracle
+// pour insérer une ligne dans la table Client
+
+// Il faut importer le paquetage java.sql pour utiliser JDBC
+package ExemplesJDBC;
+import java.sql.*;
+
+class ClientInsertJDBC
+{
+  public static void main (String args [])
+       throws SQLException, ClassNotFoundException, java.io.IOException
+  {
+    // Charger le pilote JDBC d'Oracle
+    Class.forName ("oracle.jdbc.driver.OracleDriver");
+
+    // Connection à une BD
+    Connection uneConnection =
+      DriverManager.getConnection ("jdbc:oracle:thin:@localhost:1521:orcl", "godin", "oracle");
+
+    // Création d'un énoncé associé à la Connection
+    Statement unEnoncéSQL = uneConnection.createStatement ();
+    
+    // Insertion d'une ligne dans la table Client
+    int n = unEnoncéSQL.executeUpdate
+      ("INSERT INTO CLIENT " +
+       "VALUES (100, 'G. Lemoyne', '911')");
+    System.out.println ("Nombre de lignes inserees:" + n);
+    
+    // Fermeture de énoncé et de la connexion
+    unEnoncéSQL.close();
+    uneConnection.close();
+  }
+}
