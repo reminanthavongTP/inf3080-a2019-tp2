51f5716a730e2142971da73bda4388071352e95d
reminanthavong <55417541+reminanthavong@users.noreply.github.com>
2019-12-09 15:11:54 -0500

    Update TestJava.java

--- TestJava.java
+++ TestJava.java
@@ -3,60 +3,66 @@
 // Il faut importer le paquetage java.sql pour utiliser JDBC
 //package ExemplesJDBC;
 import java.sql.*;
-//import oracle.jdbc.driver.OracleDriver;
+//import oracle.*;
 
 public class Main {
-// JDBC driver name and database URL
-   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+// JDBC driver name and database URL 
    static final String DB_URL = "jdbc:oracle:thin:@zeta2.labunix.uqam.ca:1521:baclab";
 
    //  Database credentials
    static final String USER = "gj391249";
-   static final String PASS = "NAN75614";
+   static final String PASS = "hijMdgyJ";
 
    public static void main(String[] args) throws SQLException, ClassNotFoundException, java.io.IOException
    {
-                 
-     Connection uneConnection = null; 
-    // Charger le pilote JDBC d'Oracle
-    //Class.forName ("oracle.jdbc.driver.OracleDriver");
-    //DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-   Class.forName("com.mysql.jdbc.Driver");
-    // Connection à une BD
-    uneConnection = DriverManager.getConnection(DB_URL,USER,PASS);
-
-    // Création d'un énoncé associé à la Connection
-    Statement unEnoncéSQL = uneConnection.createStatement ();
+	// Charger le pilote JDBC d'Oracle
+	    Class.forName ("oracle.jdbc.driver.OracleDriver");
     
-    // Insertion d'une ligne dans la table Client
-    int n = unEnoncéSQL.executeUpdate
-      ("INSERT INTO tp1Client" +
-       "VALUES (0,'VISA')");
-    System.out.println ("Nombre de lignes inserees:" + n);
+	 // Connection à une BD
+	    Connection uneConnection =
+	      DriverManager.getConnection (DB_URL, USER, PASS);
+
+	 // Création d'un énoncé associé à la Connection
+	    Statement unEnoncéSQL = uneConnection.createStatement ();
+    
+	    // Exécution d'un SELECT
+	    ResultSet résultatSelect = unEnoncéSQL.executeQuery
+	     ("SELECT CCOMPAGNIE, NCAMION "+
+	     "FROM tp1compagnie ");
+	    
+	    // Itérer sur les lignes du résultat du SELECT et extraire les valeurs
+	    // des colonnes dans des variables JAVA
+	    
+	    while (résultatSelect.next ()){
+	      int NCAMION = résultatSelect.getInt ("NCAMION");
+	      String CCOMPAGNIE = résultatSelect.getString ("CCOMPAGNIE");
+	      System.out.println("Exemple d'un SELECT");
+	      System.out.println ("Total Camions:" + NCAMION);
+	      System.out.println ("Nom de la compagnie:" + CCOMPAGNIE);
+	    }
+
     
      // Fermeture de énoncé et de la connexion
     unEnoncéSQL.close();
-    uneConnection.close();
+    //uneConnection.close();
     
-    // Exécution d'un SELECT
-    ResultSet résultatSelect = unEnoncéSQL.executeQuery
-     ("SELECT CCOMPAGNIE, NCAMION "+
-     "FROM tp1compagnie ");
-    
-    // Itérer sur les lignes du résultat du SELECT et extraire les valeurs
-    // des colonnes dans des variables JAVA
-    
-    while (résultatSelect.next ()){
-      int NCAMION = résultatSelect.getInt ("NCAMION");
-      String CCOMPAGNIE = résultatSelect.getString ("CCOMPAGNIE");
-      System.out.println ("Total Camions:" + NCAMION);
-      System.out.println ("Nom de la compagnie:" + CCOMPAGNIE);
-    }
+    // Création d'un appel de fonction associé à la Connection
+    CallableStatement unCall = uneConnection.prepareCall("{? = call TotalFacture (?)}");
 
+    // Spécification des paramètres d'entrée
+    unCall.setInt(2,14);
+    // Inscription de la sortie
+    unCall.registerOutParameter(1, java.sql.Types.INTEGER);
+    // Exécution de l'appel
+    unCall.execute();
     
-    // Fermeture de énoncé et de la connexion
-    unEnoncéSQL.close();
+    int leprix = unCall.getInt(1);
+    System.out.println(" ");
+    System.out.println("Exemple d'une appel fonction");
+    System.out.println("Total de la facture($) :"+leprix);
+    unCall.close();
     uneConnection.close();
+    
     
 }//end main
 }//end Main
